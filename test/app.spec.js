e4e62d82cc79c4ee4f3b15c3e48a8b6108172500
Jonathan Kleinpeter <joklein341@gmail.com>
2019-02-22 08:40:21 -0600

    initial commit

---
+++ test/app.spec.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+'use strict';
+
+const app = require('../src/app');
+
+describe('App', () => {
+  it('GET / responds with 200 containing "Hello, world!"', () => {
+    return supertest(app)
+      .get('/')
+      .expect(200, 'Hello, world!');
+  });
+});

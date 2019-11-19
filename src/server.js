14b00deb4ef0da911f5cfa9126cab4fbb0775e87
Jonathan Kleinpeter <46002837+jonkleinpet@users.noreply.github.com>
2019-11-19 09:35:49 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
-'use strict';
 const app = require('./app');
-const { PORT } = require('./config');
+const knex = require('knex');
+const { PORT, DATABASE_URL } = require('./config');
+
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL,
+});
+
+app.set('db', db);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`listening at http://localhost:${PORT}`);
 });
